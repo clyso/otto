648abfee5e27f7c079a2f5f8c05768b8239b1c1f
Joshua Blanch <joshua.blanch@clyso.com>
2025-05-05 20:46:34 +0000

    Enable automatic versioning via setuptools-scm
    Signed-off-by: Joshua Blanch <joshua.blanch@clyso.com>

--- clyq/copilot/src/clyso/__version__.py
+++ clyq/copilot/src/clyso/__version__.py
@@ -1,4 +1,6 @@
 # Copyright (C) 2025 Clyso
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-__version__ = "0.2.3"
+import importlib.metadata
+
+__version__ = importlib.metadata.version("ceph-copilot")
